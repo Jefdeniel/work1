71b699bfbc0eaa02096062acba8802d8b037c210
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2022-12-05 10:09:58 +0100

    first start for forms

--- opdrachten/atwork-1_project_1_deniel_jef/static/js/main.js
+++ opdrachten/atwork-1_project_1_deniel_jef/static/js/main.js
@@ -3,11 +3,31 @@
     init() {
       console.log("1. Initialize Application");
       this.cacheElements();
+      this.registerListener();
     },
     cacheElements() {
       console.log("2. Cache Elements");
       this.$referencesList = document.querySelector(".references-list");
       this.$referencesList.innerHTML = this.getHTMLForReferences(references);
+      this.$fromContact = document.querySelector("#fromContact");
+    },
+    registerListener() {
+      console.log("3. Register Listeners");
+      this.$fromContact.addEventListener("submit", (ev) => {
+        ev.preventDefault();
+
+        const $elements = ev.currentTarget.elements;
+
+        console.log(ev.currentTarget.elements.type);
+
+        const data = {
+          type: elements.type.value;
+          provincie: $elements.provincie.value;
+
+          // const dataAsJSONStr = json.stringify(data);
+        }
+        return false;
+      });
     },
     getHTMLForReferences(references) {
       return references
