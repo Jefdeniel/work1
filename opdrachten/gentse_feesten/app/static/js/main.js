da4ff659972f7405e84a1d615d7054f41bdfafec
pgm-jefdenie <jef.deniel@student.arteveldehs.be>
2023-01-12 20:25:58 +0100

    search parameters url

--- opdrachten/gentse_feesten/app/static/js/main.js
+++ opdrachten/gentse_feesten/app/static/js/main.js
@@ -14,3 +14,17 @@
   };
   app.initialize();
 })();
+
+// Get url information
+// get the url of the current web page
+const urlStr = window.location;
+console.log(urlStr);
+// make an url object (instance from the URL class)
+const url = new URL(urlStr);
+console.log(url);
+// Get the search params from the url object
+const searchParams = url.searchParams;
+const product = searchParams.get("product");
+const color = searchParams.get("color");
+console.log(product, color);
+// Dit volstaat dus om zaken uit uw url te halen, je kan ermee loopen etc
